2ea84d8c020be2925608d1ac1bacdcd03530af35
John Wang <johncwang@gmail.com>
2024-12-14 14:29:44 -0800

    feat: `sla`: init

---
+++ sla/sla.go
@@ -0,0 +1,66 @@
+package sla
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/grokify/govex"
+	"github.com/grokify/mogo/time/timeutil"
+)
+
+const (
+	StatusWithinSLA      = "Within SLA"
+	StatusApproachingSLA = "Approaching SLA"
+	StatusOutOfSLA       = "Out of SLA"
+)
+
+type SLAMap map[string]uint
+
+func SLAMapFedRAMP() SLAMap {
+	return map[string]uint{
+		govex.SeverityCritical: 30,
+		govex.SeverityHigh:     30,
+		govex.SeverityMedium:   90,
+		govex.SeverityLow:      180,
+	}
+}
+
+func (slaMap SLAMap) SLAStatus(severity string, dur time.Duration) (bool, error) {
+	severityParsed, err := govex.ParseSeverity(severity)
+	if err != nil {
+		return false, err
+	}
+	if len(slaMap) == 0 {
+		slaMap = SLAMapFedRAMP()
+	}
+	if slaDays, ok := slaMap[severityParsed]; !ok {
+		return false, fmt.Errorf("severity not found in SLA map (%s)", severity)
+	} else {
+		ageDays := timeutil.DurationDaysInt64(dur)
+		return ageDays < int64(slaDays), nil
+	}
+}
+
+func (slaMap SLAMap) SLAStatusTimes(severity string, startTime, evalTime time.Time) (bool, error) {
+	return slaMap.SLAStatus(severity, evalTime.Sub(startTime))
+}
+
+func (slaMap SLAMap) SLAStatusTimesString(severity string, startTime *time.Time, evalTime time.Time, unknownString string) (string, error) {
+	if startTime == nil {
+		return unknownString, nil
+	} else if withinSLA, err := slaMap.SLAStatusTimes(severity, *startTime, evalTime); err != nil {
+		return unknownString, err
+	} else if withinSLA {
+		return StatusWithinSLA, nil
+	} else {
+		return StatusOutOfSLA, nil
+	}
+}
+
+func (slaMap SLAMap) MustSLAStatusTimesString(severity string, startTime *time.Time, evalTime time.Time, unknownString string) string {
+	if status, err := slaMap.SLAStatusTimesString(severity, startTime, evalTime, unknownString); err != nil {
+		return unknownString
+	} else {
+		return status
+	}
+}
