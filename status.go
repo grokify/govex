1564acaa4fed6c7d335c5789ef7fe9217f700cd1
John Wang <johncwang@gmail.com>
2024-12-14 16:28:52 -0800

    feat: `govex`: add status constants

---
+++ status.go
@@ -0,0 +1,17 @@
+package govex
+
+const (
+	StatusIdentified    = "Identified"
+	StatusAnalyzing     = "Analyzing"
+	StatusValidated     = "Validated"
+	StatusMitigated     = "Mitigated"
+	StatusInProgress    = "In Progress"
+	StatusResolved      = "Resolved"
+	StatusRemediated    = "Remediated"
+	StatusClosed        = "Closed"
+	StatusReopened      = "Reopened"
+	StatusNotApplicable = "Not Applicable"
+	StatusFalsePositive = "False Positive"
+	StatusDeferred      = "Deferred"      // aka postponed
+	StatusRiskAccepted  = "Risk Accepted" // aka ignored
+)
