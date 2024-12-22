cc7a0cf83ad99b4fd85b00b875211e65d0b300f3
John Wang <johncwang@gmail.com>
2024-12-22 02:44:12 -0800

    feat: `govex`: add SCA table defs `TableColumnDefinitionSetSCA()`

--- vulnerabilities_table.go
+++ vulnerabilities_table.go
@@ -2,6 +2,7 @@ package govex
 
 import (
 	"github.com/grokify/gocharts/v2/data/table"
+	"github.com/grokify/govex/severity"
 )
 
 func (vs *Vulnerabilities) Table(colDefs table.ColumnDefinitionSet, opts *ValueOpts) (*table.Table, error) {
@@ -20,7 +21,7 @@ func TableColumnDefinitionSetSAST() table.ColumnDefinitionSet {
 			{
 				Name:         FieldSeverity,
 				SourceName:   FieldSeverity,
-				DefaultValue: SeverityUnknown,
+				DefaultValue: severity.SeverityUnknown,
 			},
 			{
 				Name:         FieldCategory,
@@ -41,19 +42,63 @@ func TableColumnDefinitionSetSAST() table.ColumnDefinitionSet {
 				Name:       FieldDescription,
 				SourceName: FieldDescription,
 			},
-			{
+			{ // SAST
 				Name:       FieldLocationPath,
 				SourceName: FieldLocationPath,
 			},
-			{
+			{ // SAST
 				Name:       FieldLocationLineStart,
 				SourceName: FieldLocationLineStart,
 				Format:     table.FormatInt,
 			},
-			{
+			{ // SAST
 				Name:       FieldLocationLineEnd,
 				SourceName: FieldLocationLineEnd,
 				Format:     table.FormatInt,
+			},
+		},
+	}
+}
+
+func TableColumnDefinitionSetSCA() table.ColumnDefinitionSet {
+	return table.ColumnDefinitionSet{
+		DefaultFormat: table.FormatString,
+		Definitions: []table.ColumnDefinition{
+			{
+				Name:         FieldSeverity,
+				SourceName:   FieldSeverity,
+				DefaultValue: severity.SeverityUnknown,
+			},
+			{
+				Name:         FieldCategory,
+				SourceName:   FieldCategory,
+				DefaultValue: CategorySAST,
+			},
+			{
+				Name:       FieldName,
+				SourceName: FieldNameWithURL,
+				Format:     table.FormatURL,
+			},
+			{
+				Name:       FieldReferenceURL,
+				SourceName: FieldReferenceURL,
+				Format:     table.FormatURL,
+			},
+			{
+				Name:       FieldDescription,
+				SourceName: FieldDescription,
+			},
+			{ // SCA
+				Name:       FieldLibraryName,
+				SourceName: FieldLibraryName,
+			},
+			{ // SCA
+				Name:       FieldLibraryVersion,
+				SourceName: FieldLibraryVersion,
+			},
+			{ // SCA
+				Name:       FieldLibraryVersionFixed,
+				SourceName: FieldLibraryVersionFixed,
 			},
 		},
 	}
