4f91b3b6c0310c78856c6de482af940258406f10
John Wang <johncwang@gmail.com>
2024-12-25 03:25:23 -0800

    feat: `govex`: add `TableColumnDefinitionSetSASTSCA()`; update fields

--- vulnerabilities_table.go
+++ vulnerabilities_table.go
@@ -130,3 +130,61 @@ func TableColumnDefinitionSetSCA() table.ColumnDefinitionSet {
 		},
 	}
 }
+
+func TableColumnDefinitionSetSASTSCA() table.ColumnDefinitionSet {
+	return table.ColumnDefinitionSet{
+		DefaultFormat: table.FormatString,
+		Definitions: []table.ColumnDefinition{
+			{
+				Name:         FieldSeverity,
+				SourceName:   FieldSeverity,
+				DefaultValue: severity.SeverityUnknown,
+			},
+			{
+				Name:         FieldCategory,
+				SourceName:   FieldCategory,
+				DefaultValue: CategorySAST,
+			},
+			{
+				Name:       FieldName,
+				SourceName: FieldNameWithURL,
+				Format:     table.FormatURL,
+			},
+			{
+				Name:       FieldReferenceURL,
+				SourceName: FieldReferenceURL,
+				Format:     table.FormatURL,
+			},
+			{
+				Name:       FieldDescription,
+				SourceName: FieldDescription,
+			},
+			{ // SAST
+				Name:       FieldLocationPath,
+				SourceName: FieldLocationPath,
+			},
+			{ // SAST
+				Name:       FieldLocationLineStart,
+				SourceName: FieldLocationLineStart,
+				Format:     table.FormatInt,
+			},
+			{ // SAST
+				Name:       FieldLocationLineEnd,
+				SourceName: FieldLocationLineEnd,
+				Format:     table.FormatInt,
+			},
+			{ // SCA
+				Name:       FieldLibraryName,
+				SourceName: FieldLibraryName,
+			},
+			{ // SCA
+				Name:       FieldLibraryVersion,
+				SourceName: FieldLibraryVersion,
+			},
+			{ // SCA
+				Name:       FieldLibraryVersionFixed,
+				SourceName: FieldLibraryVersionFixed,
+			},
+		},
+	}
+}
