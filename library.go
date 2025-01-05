9dcecb280e5de8645f86fb3a58c9998555330307
John Wang <johncwang@gmail.com>
2025-01-04 20:22:19 -0800

    feat: `Library`: add `Type` field to correspond with Grype

--- library.go
+++ library.go
@@ -3,6 +3,7 @@ package govex
 type Library struct {
 	Name         string `json:"name"`
 	Description  string `json:"description"`
+	Type         string `json:"type"` // corresponds with Grype.
 	Version      string `json:"version"`
 	VersionFixed string `json:"versionFixed"`
 }
