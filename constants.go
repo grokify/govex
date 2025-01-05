97aaf488474fd4d6c4b362b4c66d3bb6739db598
John Wang <johncwang@gmail.com>
2025-01-04 18:00:56 -0800

    feat: `constants.go`: add

--- constants.go
+++ constants.go
@@ -2,13 +2,21 @@ package govex
 
 const (
 	// Vulnerability source category.
-	CategoryAntiVirus = "Anti-Virus"
-	CategoryContainer = "Container"
-	CategoryDAST      = "DAST"
-	CategoryPentest   = "Pentest"
-	CategorySAST      = "SAST"
-	CategorySCA       = "SCA"
-	CategorySecret    = "Secret"
+	CategoryAntiVirus     = "Anti-Virus"
+	CategoryCICD          = "CI/CD"
+	CategoryCloudSecurity = "Cloud Security"
+	CategoryContainer     = "Container"
+	CategoryCSPM          = "CSPM"
+	CategoryDAST          = "DAST"
+	CategoryDevProcess    = "Dev Process"
+	CategoryIaC           = "IaC"
+	CategoryPentest       = "Pentest"
+	CategoryRedTeam       = "Red Team"
+	CategorySAST          = "SAST"
+	CategorySCA           = "SCA"
+	CategorySCI           = "Supply Chain Integrity"
+	CategorySecrets       = "Secrets"
+	CategorySecurityLogs  = "Security Logs"
 
 	Priority1 = "Priority 1"
 	Priority2 = "Priority 2"
@@ -20,3 +28,23 @@ const (
 
 	ReportName = "AppSec Scan Report"
 )
+
+func CategoriesSorted() []string {
+	return []string{
+		CategoryCICD,
+		CategoryDevProcess,
+		CategorySecrets,
+		CategorySAST,
+		CategorySCA,
+		CategoryIaC,
+		CategoryCSPM,
+		CategoryContainer,
+		CategorySCI,
+		CategoryCloudSecurity,
+		CategoryAntiVirus,
+		CategorySecurityLogs,
+		CategoryDAST,
+		CategoryPentest,
+		CategoryRedTeam,
+	}
+}
