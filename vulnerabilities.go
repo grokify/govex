dd446ab44f1e0a638391c3506d0cb0190d01d477
John Wang <johncwang@gmail.com>
2025-12-01 00:07:22 -0800

    refactor: `govex.Vulnerabilites{}`, migrate files

--- vulnerabilities.go
+++ vulnerabilities.go
@@ -6,11 +6,9 @@ import (
 	"sort"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/grokify/gocharts/v2/data/histogram"
 	"github.com/grokify/gocharts/v2/data/table"
-	"github.com/grokify/mogo/type/maputil"
 	"github.com/grokify/mogo/type/slicesutil"
 	"github.com/grokify/mogo/type/stringsutil"
 
@@ -106,63 +104,10 @@ func (vs *Vulnerabilities) OrderedListMarkdownLines(opts *ValueOptions) []string
 	return lines
 }
 
-func (vs *Vulnerabilities) SeverityHistogram() histogram.Histogram {
-	h := histogram.NewHistogram("")
-	h.Order = severity.SeveritiesAll()
-	for _, vn := range *vs {
-		h.Add(vn.Severity, 1)
-	}
-	h.Order = severity.SeveritiesAll()
-	return *h
-}
-
-func (vs *Vulnerabilities) SeverityCounts() maputil.Records {
-	h := vs.SeverityHistogram()
-	return h.ItemValuesOrdered()
-}
-
-func (vs *Vulnerabilities) SeverityCountsString(sep string) string {
-	recs := vs.SeverityCounts()
-	return recs.String(sep)
-}
-
 func (vs *Vulnerabilities) SortByID() {
 	slices.SortFunc(*vs, func(a, b Vulnerability) int {
 		return cmp.Compare(a.ID, b.ID)
 	})
-}
-
-func (vs *Vulnerabilities) SeverityStatsSetByModule(slaPolicy severity.SLAPolicy, slaCalcTime time.Time, unknownModule string) (severity.SeverityStatsSet, error) {
-	statsSet := severity.NewSeverityStatsSet()
-	for _, vn := range *vs {
-		if vn.SLATimeStart == nil {
-			continue
-		} else if vn.SLATimeStart.Before(slaCalcTime) {
-			if err := statsSet.Add(slaPolicy, vn.Module, vn.Severity, slaCalcTime.Sub(*vn.SLATimeStart)); err != nil {
-				return statsSet, err
-			}
-		}
-	}
-	return statsSet, nil
-}
-
-func (vs *Vulnerabilities) SeverityStatsSetBySeverity(slaPolicy severity.SLAPolicy, slaCalcTime time.Time, unknownModule string) (severity.SeverityStatsSet, error) {
-	statsSet := severity.NewSeverityStatsSet()
-	statsSet.Order = []string{
-		severity.SeverityCritical,
-		severity.SeverityHigh,
-		severity.SeverityMedium,
-		severity.SeverityLow}
-	for _, vn := range *vs {
-		if vn.SLATimeStart == nil {
-			continue
-		} else if vn.SLATimeStart.Before(slaCalcTime) {
-			if err := statsSet.Add(slaPolicy, vn.Severity, vn.Severity, slaCalcTime.Sub(*vn.SLATimeStart)); err != nil {
-				return statsSet, err
-			}
-		}
-	}
-	return statsSet, nil
 }
 
 func (vs *Vulnerabilities) SortBySLATimeStart() {
--- vulnerabilities_stats.go
+++ vulnerabilities_stats.go
@@ -3,8 +3,31 @@ package govex
 import (
 	"time"
 
+	"github.com/grokify/gocharts/v2/data/histogram"
+	"github.com/grokify/mogo/type/maputil"
+
 	"github.com/grokify/govex/severity"
 )
+
+func (vs *Vulnerabilities) SeverityCounts() maputil.Records {
+	h := vs.SeverityHistogram()
+	return h.ItemValuesOrdered()
+}
+
+func (vs *Vulnerabilities) SeverityCountsString(sep string) string {
+	recs := vs.SeverityCounts()
+	return recs.String(sep)
+}
+
+func (vs *Vulnerabilities) SeverityHistogram() histogram.Histogram {
+	h := histogram.NewHistogram("")
+	h.Order = severity.SeveritiesAll()
+	for _, vn := range *vs {
+		h.Add(vn.Severity, 1)
+	}
+	h.Order = severity.SeveritiesAll()
+	return *h
+}
 
 func (vs *Vulnerabilities) SeverityStats(slaPolicy severity.SLAPolicy, slaCalcTime time.Time) (*severity.SeverityStats, error) {
 	statsSet := severity.NewSeverityStats()
@@ -18,4 +41,37 @@ func (vs *Vulnerabilities) SeverityStats(slaPolicy severity.SLAPolicy, slaCalcTi
 		}
 	}
 	return &statsSet, nil
+}
+
+func (vs *Vulnerabilities) SeverityStatsSetByModule(slaPolicy severity.SLAPolicy, slaCalcTime time.Time, unknownModule string) (severity.SeverityStatsSet, error) {
+	statsSet := severity.NewSeverityStatsSet()
+	for _, vn := range *vs {
+		if vn.SLATimeStart == nil {
+			continue
+		} else if vn.SLATimeStart.Before(slaCalcTime) {
+			if err := statsSet.Add(slaPolicy, vn.Module, vn.Severity, slaCalcTime.Sub(*vn.SLATimeStart)); err != nil {
+				return statsSet, err
+			}
+		}
+	}
+	return statsSet, nil
+}
+
+func (vs *Vulnerabilities) SeverityStatsSetBySeverity(slaPolicy severity.SLAPolicy, slaCalcTime time.Time, unknownModule string) (severity.SeverityStatsSet, error) {
+	statsSet := severity.NewSeverityStatsSet()
+	statsSet.Order = []string{
+		severity.SeverityCritical,
+		severity.SeverityHigh,
+		severity.SeverityMedium,
+		severity.SeverityLow}
+	for _, vn := range *vs {
+		if vn.SLATimeStart == nil {
+			continue
+		} else if vn.SLATimeStart.Before(slaCalcTime) {
+			if err := statsSet.Add(slaPolicy, vn.Severity, vn.Severity, slaCalcTime.Sub(*vn.SLATimeStart)); err != nil {
+				return statsSet, err
+			}
+		}
+	}
+	return statsSet, nil
 }
